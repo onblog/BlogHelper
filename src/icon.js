186a1a9938c42b06bb23dbc4dcccfabf44b2577e
onblog <codeblog@qq.com>
2020-01-11 02:07:23 +0800

    图标问题

--- src/icon.js
+++ src/icon.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 // 系统托盘图标目录 __dirname:主进程文件所在目录
-const iconDir = path.join(__dirname, '../build');
+const iconDir = path.normalize(path.join(__dirname, 'resource'));
 // 按平台选择，mac是18px的倍数，win是16px的倍数
 const iconName = process.platform === 'win32' ? 'star-win.png' : 'star-mac.png'
 const proIconName = process.platform === 'win32' ? 'process-win.png' : 'process-mac.png'
