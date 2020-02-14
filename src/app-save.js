bc5ea4517faabfa2cd22eaa06d57f581c726bf04
yueshutong <yster@foxmail.com>
2020-02-14 10:01:11 +0800

    文章整理后保存在原文件或者剪贴板，优化流程提示

--- src/app-save.js
+++ src/app-save.js
@@ -6,28 +6,22 @@ let numOne = 0
 let numTwo = 0
 
 // 操作完成，保存在新文件还是剪贴板？
-exports.saveNewFileOrClipboard = function saveNewFileOrClipboard(result, content, output, i) {
+exports.saveNewFileOrClipboard = function saveNewFileOrClipboard(file, content, i) {
     if (!i) {
         i = 0
     }
     if (i === 0) {
         // 1.提示保存
-        numOne = dialog.showMessageBoxSync({message: '操作完成，保存在', buttons: ['新文件', '剪贴板']})
+        numOne = dialog.showMessageBoxSync({message: '操作完成，保存在', buttons: ['原文件', '剪贴板']})
     }
     if (numOne === 0) {
         // 2.写入新文档
-        let filename = result.title + result.extname
-        const dirname = path.join(result.dirname, output)
-        if (!fs.existsSync(dirname)) {
-            fs.mkdirSync(dirname, {recursive: true})
-        }
-        let filepath = path.join(dirname, filename)
-        fs.writeFileSync(filepath, content)
+        fs.writeFileSync(file.filepath, content)
         if (i === 0) {
             numTwo = dialog.showMessageBoxSync({message: '保存成功，是否打开新文档？', buttons: ['不了,谢谢', '打开']})
         }
         if (numTwo === 1) {
-            shell.openItem(filepath)
+            shell.openItem(file.filepath)
         }
     } else if (numOne === 1) {
         // 3.写入剪贴板
