2aada152c64546235c6d7fe2c9567609308b51d4
onblog <codeblog@qq.com>
2021-01-03 15:30:49 +0800

    update

--- src/common/app-link.js
+++ src/common/app-link.js
@@ -1,4 +1,4 @@
 exports.url = 'https://github.com/onblog/BlogHelper/releases';
 exports.link = 'https://github.com/onblog/BlogHelper';
 exports.issues = 'https://github.com/onblog/BlogHelper/issues';
-exports.download = 'http://disk.ystblog.com';
+exports.download = 'https://github.com/onblog/BlogHelper/releases';
